b0c78fff798d36f5f35b5e50c80e54251e1c0192
HarryStemwork <162728191+HarryStemwork@users.noreply.github.com>
2024-08-19 01:29:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ dst_path = #what is the dst path?
 #how to move from beforeCal to afterCal?
 
 discount_f = open('', 'r') #how to open discount.txt
-finish_f = open('','w')
-for num in discount_f.readlines:
+finish_f = open('','a')
+for num in discount_f.readlines():
     #How to calculate the discount and saving to the new finish.txt
     #remeber to round to 2 decimal places
 
