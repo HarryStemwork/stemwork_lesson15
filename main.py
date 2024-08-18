1c2129fbb4921d496ad111cb85a045c7712f6264
HarryStemwork <162728191+HarryStemwork@users.noreply.github.com>
2024-08-19 01:21:46 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import #what should be imported
+
+src_path = #what is the source path?
+dst_path = #what is the dst path?
+
+#how to move from beforeCal to afterCal?
+
+discount_f = open('', 'r') #how to open discount.txt
+finish_f = open('','w')
+for num in discount_f.readlines:
+    #How to calculate the discount and saving to the new finish.txt
+    #remeber to round to 2 decimal places
+
+#how to zip afterCal
